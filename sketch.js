b831af68c8c533b70ad41a02ef70e7359c852860
Dhruvmenon48 <75004959+Dhruvmenon48@users.noreply.github.com>
2020-12-01 20:09:25 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,37 +1,38 @@
-var fixedRect, movingRect;
-var gameObject1, gameObject2, gameObject3, gameObject4;
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
 
-function setup() {
-  createCanvas(1200,800);
-  fixedRect = createSprite(600, 400, 50, 80);
-  fixedRect.shapeColor = "green";
- 
-  movingRect = createSprite(800, 400,80,30);
-  movingRect.shapeColor = "green";
+var engine, world;
+var ground, ball;
 
-  gameObject1 = createSprite(100, 100, 50, 50);
-  gameObject1.shapeColor = "green";
-  gameObject2 = createSprite(200, 100, 50, 50);
-  gameObject2.shapeColor = "green";
-  gameObject3 = createSprite(300, 100, 50, 50);
-  gameObject3.shapeColor = "green";
-  gameObject4 = createSprite(400, 100, 50, 50);
-  gameObject4.shapeColor = "green";
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    var ground_options ={
+        isStatic: true
+    }
+
+    ground = Bodies.rectangle(200,390,200,20,ground_options);
+    World.add(world,ground);
+
+    var ball_options ={
+        restitution: 1.0
+    }
+
+    ball = Bodies.circle(200,100,20, ball_options);
+    World.add(world,ball);
+
+    console.log(ground);
 }
 
-function draw() {
-  background(0,0,0);  
-  movingRect.x = World.mouseX;
-  movingRect.y = World.mouseY;
+function draw(){
+    background(0);
+    Engine.update(engine);
+    rectMode(CENTER);
+    rect(ground.position.x,ground.position.y,400,20);
 
-  if(isTouching(movingRect, gameObject1)){
-    movingRect.shapeColor = "blue";
-    gameObject1.shapeColor = "blue";
-  }
-  else {
-    movingRect.shapeColor = "green";
-    gameObject1.shapeColor = "green";
-  }
-  drawSprites();
+    ellipseMode(RADIUS);
+    ellipse(ball.position.x, ball.position.y, 20, 20);
 }
-
